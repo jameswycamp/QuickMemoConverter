66e27fa9897a64ea8c9122464e391c99f7b247eb
James Camp <James.Wy.Camp@gmail.com>
2020-11-24 10:41:58 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace QuickMemoConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string memofile = @"C:\temp\memoinfo.jlqm";
+
+            if(File.Exists(memofile))
+            {
+
+                string note = File.ReadAllText(memofile);
+
+                using (JsonDocument document = JsonDocument.Parse(note))
+                {
+                    JsonElement root = document.RootElement;
+
+                    JsonElement memoCategory = root.GetProperty("Category");
+                    memoCategory.TryGetProperty("CategoryName",out JsonElement categoryElement);
+
+                    Console.WriteLine(categoryElement.ToString());
+
+                    JsonElement memoContentList = root.GetProperty("MemoObjectList");
+
+                    foreach (var memoContent in memoContentList.EnumerateArray())
+                    {
+                        if (memoContent.TryGetProperty("DescRaw",out JsonElement contentElement);)
+                    }
+
+                    
+                    
+
+                    Console.WriteLine(contentElement.ToString());
+                }
+
+                // Console.WriteLine(note);
+            }
+        }
+    }
+}
